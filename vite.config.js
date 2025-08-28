ca2fe4bc661e1efe7f6be311a1611e987e9f0c03
红楼 <51036409+asd15555866@users.noreply.github.com>
2025-08-29 05:05:38 +0800

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  build: {
-    target: 'node18', // 指定 Node.js 版本
-    ssr: true,       // 启用服务端渲染，允许使用 Node.js API
-  },
-});
